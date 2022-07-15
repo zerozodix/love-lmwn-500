e6a9cec06219502f30ab558437ac01841d9ba992
zerozodix <zerozodix@gmail.com>
2022-07-15 09:41:53 +0700

    init project

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	//var whatIsIt string
+
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+
+	fmt.Printf("%q\n", sd)
+
+	ans := reverse(string(sd))
+
+	fmt.Printf("%q\n", ans)
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
